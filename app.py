8a528292e1cc25bcf9827c2489a818d2da490d6b
HectorFpps <71526987+HectorFpps@users.noreply.github.com>
2022-01-12 11:31:52 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import yfinance
+import pandas as pd
+
+ticker = str(st.text_input("-Input Desired Ticker (Yahoo Finance)"))
+
+while True:
+    df = pd.DataFrame()
+    df = df.ta.ticker(ticker, period="100d", interval="1h")
+    st.line_chart(df.Close)
+
